a319704e77b0b47eabc981f5997dfa2f67600614
Tatiane Belo <tatianebelo.ads@gmail.com>
2018-11-23 23:52:11 -0300

    adicionando Atividade 23/11

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+
+public class Calculadora {
+
+	public static double somar(double v1, double v2) {
+		return v1 + v2;
+	}
+	
+	public static double subtrair(double v1, double v2) {
+		return v1 - v2;
+	}
+	
+	public static double multiplicar(double v1, double v2) {
+		return v1 * v2;
+	}
+	
+	public static double dividir(double v1, double v2) {
+		return v1 / v2;
+	}
+}
---
+++ src/Calculadora/Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+	
+		System.out.println(Calculadora.somar(1,1));
+
+		System.out.println(Calculadora.multiplicar(2, 2));
+
+	}
+}
